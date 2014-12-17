6ae086d451ecc2c9f8242adfe15437b7b720ba99
Curtis <tyrantt47@hotmail.com>
2014-12-16 20:23:18 -0800

    finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title = "Home"
+  	@word = "Home Page"
   end
 
   def help
+  	@title = "Help"
+  end
+
+  def about
+  	@title = "About"
   end
 end
